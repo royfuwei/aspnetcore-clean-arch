e90d39bd1866af501aee16d8cf1a599923f00f27
royfuwei <royfuwei@gmail.com>
2023-04-10 23:36:49 +0800

    [feat] Domain add WeatherForecast repository interface

--- src/Domain/AggregatesModels/WeatherForecastAggregate/Repositories/IWeatherForecastRepository.cs
+++ src/Domain/AggregatesModels/WeatherForecastAggregate/Repositories/IWeatherForecastRepository.cs
@@ -2,5 +2,12 @@ namespace CleanArch.Domain.AggregatesModels.WeatherForecastAggregate.Repositorie
 
 public interface IWeatherForecastRepository : IRepository<WeatherForecast>
 {
+    Task<WeatherForecast> Add(WeatherForecast item);
+
+    void Update(WeatherForecast item);
+
+    Task<WeatherForecast> GetAsync(int id);
     
+    Task<IEnumerable<WeatherForecast>> GetAllAsync();
+
 }
