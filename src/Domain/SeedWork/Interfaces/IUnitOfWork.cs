43473c629e25bf1c0c669cc5ba09ae26431d66ab
royfuwei <royfuwei@gmail.com>
2023-04-11 12:55:53 +0800

    [feat] Domain remove unuse package

--- src/Domain/SeedWork/Interfaces/IUnitOfWork.cs
+++ src/Domain/SeedWork/Interfaces/IUnitOfWork.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CleanArch.Domain.SeedWork.Interfaces;
 
 public interface IUnitOfWork : IDisposable
