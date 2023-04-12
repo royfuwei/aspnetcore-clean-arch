ad375524cdba667d1df9b82a6d251004b0304ba7
royfuwei <royfuwei@gmail.com>
2023-04-12 14:07:43 +0800

    [feat] Infra add package EntityFrameworkCore... , add GlobalUsing

---
+++ src/Infrastructure/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using MediatR;
+global using CleanArch.Domain.AggregatesModels.WeatherForecastAggregate;
+global using Microsoft.EntityFrameworkCore;
+global using CleanArch.Domain.SeedWork;
+global using CleanArch.Domain.SeedWork.Interfaces;
