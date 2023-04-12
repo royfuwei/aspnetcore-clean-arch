3ccfbe14f6af6654f7c365e770316d22758fe0a2
royfuwei <royfuwei@gmail.com>
2023-04-12 14:13:33 +0800

    [feat] Infra update ConfigureService.cs, add AddCustomDbContext

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,15 +1,14 @@
 using System.Reflection;
+using CleanArch.Infrastructure.Persistence.EFCore.Contexts;
 using CleanArch.Infrastructure.Persistence.InMemory;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-// namespace Microsoft.Extensions.DependencyInjection;
 namespace CleanArch.Infrastructure;
 
-// public static class ConfigureServices
-public static class InfrastructureConfigureServices
+public static class ConfigureServices
 {
-    // public static IServiceCollection AddInfrastructureServices(this IServiceCollection service)
-    public static IServiceCollection AddInfrastructureServices(IServiceCollection service)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection service, IConfiguration configuration)
     {
         // 原生DI MediatR
         service.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
@@ -17,4 +16,28 @@ public static class InfrastructureConfigureServices
 
         return service;
     }
+
+    public static IServiceCollection AddCustomDbContext(this IServiceCollection service, IConfiguration configuration)
+    {
+        if (bool.Parse(configuration["UseInMemoryDatabase"]!))
+        {
+            service.AddDbContext<WeatherForecastDbContext>(options => {
+                options.UseInMemoryDatabase("CleanArchDb");
+            });
+        } 
+        else
+        {
+            service.AddDbContext<WeatherForecastDbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(typeof(WeatherForecastDbContext).Assembly.FullName);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+            });
+        }
+
+        return service;
+    }
+    
 }
