7ee5029dd376fe0fd7388fb45a830e22a171166b
royfuwei <royfuwei@gmail.com>
2023-04-12 14:00:27 +0800

    [chore] Application ConfigureService use this IServiceCollection service

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -1,16 +1,14 @@
 using System.Reflection;
 using CleanArch.Application.Modules.WeatherForecasts.Repositories.InMemory;
 using CleanArch.Domain.AggregatesModels.WeatherForecastAggregate.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-// namespace Microsoft.Extensions.DependencyInjection;
 namespace CleanArch.Application;
 
-// public static class ConfigureServices
-public static class ApplicationConfigureServices
+public static class ConfigureServices
 {
-    // public static IServiceCollection AddApplicationServices(this IServiceCollection service)
-    public static IServiceCollection AddApplicationServices(IServiceCollection service)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection service, IConfiguration Configuration)
     {
         // 原生DI MediatR
         service.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
