c38a9bcc86a4011b473384bb7826739a277bd500
royfuwei <royfuwei@gmail.com>
2023-04-13 10:49:59 +0800

    [feat] API dotnet ef migrations 出來的資料

---
+++ src/Services/API/Infrastructure/EFCore/Migrations/20230413024049_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "weatherforecast");
+
+            migrationBuilder.CreateSequence(
+                name: "weatherforecastseq",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "weatherForecasts",
+                schema: "weatherforecast",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TemperatureC = table.Column<int>(type: "int", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_weatherForecasts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "weatherForecasts",
+                schema: "weatherforecast");
+
+            migrationBuilder.DropSequence(
+                name: "weatherforecastseq");
+        }
+    }
+}
